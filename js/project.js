189b877d12e9e9e607b30a1a2f781c6ec4df1322
zozer Mbula <63924981+Zozer253@users.noreply.github.com>
2022-04-18 02:26:45 -0700

    Add files via upload

---
+++ js/project.js
@@ -0,0 +1,28 @@
+let projects = [
+    {
+        name: "project one",
+        tags: "#system, #networking, #python, #devops, #frontend",
+        image: "project-one.jpg",
+    },
+    {
+        name: "project two",
+        tags: "#system, #networking",
+        image: "project-two.jpg",
+    },
+    {
+        name: "project three",
+        tags: "#python, #frontend",
+        image: "project-one.jpg",
+    },
+    {
+        name: "project four",
+        tags: "#python, #devops",
+        image: "project-one.jpg",
+    },
+    {
+        name: "project five",
+        tags: "#python, #frontend",
+        image: "project-one.jpg",
+    }
+
+]
